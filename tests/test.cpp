4f7d91fc617eea234a1bb4f49ab116f3b5c1c833
khva <khvaonline@gmail.com>
2023-01-13 23:16:36 +0300

    add CMakeLists

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,16 +1,20 @@
-#include "base64.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include <cassert>
 #include <string>
 #include <vector>
 
-size_t test_nums()
+#include "doctest/doctest.h"
+#include "base64.h"
+
+TEST_CASE("test_nums")
 {
 	const std::string s = "012345678910";
 	const size_t enc_size = base64::calc_encoded_size(s.size());
 
-	std::string res{};
+	std::string res;
 	res.resize(enc_size);
-	size_t N = base64::encode(s, res);
-	assert(res == "MDEyMzQ1Njc4OTEw");
-	return N;
+	const size_t flag = base64::encode(s, res);
+	REQUIRE(res == "MDEyMzQ1Njc4OTEw");
+	REQUIRE(flag == 0);
 }
